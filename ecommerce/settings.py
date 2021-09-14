ec558b79f31335befcfd919e7b9637a243296d06
Benji <73948537+Fletcher-Benji@users.noreply.github.com>
2021-09-14 11:35:47 +0100

    Settings issue
    Python Django: You're using the static files app without having set the STATIC_ROOT setting

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -129,3 +129,7 @@ EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
